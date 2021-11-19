d9825c64b691a019f3845fd714e45c3c08af847c
Niladri <niladri.1994@gmail.com>
2021-11-19 13:10:30 +0530

    Checkin for quiz 3

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -9,6 +9,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 
 from utils.utils import flatten_images, preprocess, create_splits, train_and_save_model, get_best_model_path, get_best_model_metrics, model_test
 
+sys.path.insert(1, '/home/niladri/mlops/mnist-example/mnist-example/utils')
+import utils
+
+clf_best_svm = utils.utils.load('/home/niladri/mlops/mnist-example/models/SVM_0.001.pkl')
+clf_best_decision_tree = utils.utils.load('/home/niladri/mlops/mnist-example/models/DecisionTree_18.0.pkl')
+
 
 def test_equality():
 
@@ -23,7 +29,7 @@ def test_model_writing():
     flattened_images, digits = flatten_images()
     rescaled_images = preprocess(flattened_images, rescale_factor = rescale_factor)
     X_train, X_test, X_validation, y_train, y_test, y_validation = create_splits(rescaled_images, digits.target, test_size, validation_size_from_test_size)
-    df = train_and_save_model(gamma, X_train, y_train, X_validation, y_validation)
+    df = train_and_save_model('SVM', gamma, X_train, y_train, X_validation, y_validation)
     model_path = get_best_model_path(df)
 
     assert os.path.isfile(model_path)
@@ -37,7 +43,7 @@ def test_small_data_overfit_checking():
     flattened_images, digits = flatten_images()
     rescaled_images = preprocess(flattened_images, rescale_factor = rescale_factor)
     X_train, X_test, X_validation, y_train, y_test, y_validation = create_splits(rescaled_images, digits.target, test_size, validation_size_from_test_size)
-    df = train_and_save_model(gamma, X_train[:subsampling], y_train[:subsampling], X_train[:subsampling], y_train[:subsampling])
+    df = train_and_save_model('SVM', gamma, X_train[:subsampling], y_train[:subsampling], X_train[:subsampling], y_train[:subsampling])
     model_path = get_best_model_path(df)
     accuracy_train, f1_score_train = get_best_model_metrics(df)
     accuracy_test, f1_score_test = model_test(model_path, X_test, y_test)
@@ -84,3 +90,88 @@ def test_create_split_9_samples():
     assert len(X_validation) == 2 
     assert len(X_test) == 1 
     assert len(X_train) + len(X_validation) + len(X_test) == 9
+
+def test_digit_correct_0():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+
+    assert clf_best_svm.predict(X)[0] == 0
+    assert clf_best_decision_tree.predict(X)[0] == 0
+
+def test_digit_correct_1():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+
+    assert clf_best_svm.predict(X)[1] == 1
+    assert clf_best_decision_tree.predict(X)[1] == 1
+
+def test_digit_correct_2():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+
+    assert clf_best_svm.predict(X)[2] == 2
+    assert clf_best_decision_tree.predict(X)[2] == 2
+
+def test_digit_correct_3():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+    assert clf_best_svm.predict(X)[3] == 3
+    assert clf_best_decision_tree.predict(X)[3] == 3
+
+def test_digit_correct_4():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+    assert clf_best_svm.predict(X)[4] == 4
+    assert clf_best_decision_tree.predict(X)[4] == 4
+
+def test_digit_correct_5():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+    assert clf_best_svm.predict(X)[15] == 5
+    assert clf_best_decision_tree.predict(X)[15] == 5
+
+def test_digit_correct_6():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+    assert clf_best_svm.predict(X)[6] == 6
+    assert clf_best_decision_tree.predict(X)[6] == 6
+
+def test_digit_correct_7():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+    assert clf_best_svm.predict(X)[7] == 7
+    assert clf_best_decision_tree.predict(X)[7] == 7
+
+def test_digit_correct_8():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+    assert clf_best_svm.predict(X)[8] == 8
+    assert clf_best_decision_tree.predict(X)[8] == 8
+
+def test_digit_correct_9():
+
+    digits = datasets.load_digits()
+    n_samples = len(digits.images)
+    X = digits.images.reshape((n_samples, -1))
+
+    assert clf_best_svm.predict(X)[9] == 9
+    assert clf_best_decision_tree.predict(X)[9] == 9
+
