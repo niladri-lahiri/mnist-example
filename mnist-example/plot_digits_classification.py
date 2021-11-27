1bf59b971827c3e163573ab137c05b6b9be69723
Niladri <niladri.1994@gmail.com>
2021-11-27 19:38:55 +0530

    Final Exam Initial Commit

--- mnist-example/plot_digits_classification.py
+++ mnist-example/plot_digits_classification.py
@@ -16,19 +16,22 @@ import sys
 from utils.utils import flatten_images, preprocess, create_splits, train_and_save_model, get_best_model_path, model_test, print_data_frame, print_metrics
 
 #Get values from command line
-rescale_factor = int(sys.argv[1])
-gamma_values = map(float, sys.argv[2].strip('[]').split(','))
-depth_value = map(float, sys.argv[3].strip('[]').split(','))
-test_size = float(sys.argv[4])
-validation_size_from_test_size = float(sys.argv[5])/float(sys.argv[4])
+rescale_factor = 1
+#gamma_values = map(float, sys.argv[2].strip('[]').split(','))
+hyperparameters = [[ 16, 'log2', 'gini'], [16, 'log2', 'gini'], [16, 'log2', 'gini']]
+#hyperparameters = [[ 4, 'sqrt', 'gini'], [8, 'auto', 'entropy'], [16, 'log2', 'gini']]
+test_size = 0.3
+validation_size_from_test_size = 0.15
 
 flattened_images, digits = flatten_images()
 rescaled_images = preprocess(flattened_images, rescale_factor = rescale_factor)
 X_train, X_test, X_validation, y_train, y_test, y_validation = create_splits(rescaled_images, digits.target, test_size, validation_size_from_test_size)
-classifiers = {'SVM' : gamma_values, 'DecisionTree' : depth_value}
+#classifiers = {'SVM' : gamma_values, 'DecisionTree' : depth_value}
+classifiers = {'DecisionTree' : hyperparameters}
 for clf_name in classifiers:
-    df = train_and_save_model(clf_name, classifiers[clf_name], X_train, y_train, X_validation, y_validation)
-    model_path = get_best_model_path(df)
-    print_data_frame(clf_name, df)
-    accuracy_test, f1_score_test = model_test(model_path, X_test, y_test)
-    print_metrics(accuracy_test, f1_score_test)
+    df = train_and_save_model(clf_name, classifiers[clf_name], X_train, y_train, X_validation, y_validation, X_test, y_test)
+
+    #model_path = get_best_model_path(df)
+    #print_data_frame(clf_name, df)
+    #accuracy_test, f1_score_test = model_test(model_path, X_test, y_test)
+    #print_metrics(accuracy_test, f1_score_test)
--- mnist-example/utils/utils.py
+++ mnist-example/utils/utils.py
@@ -40,9 +40,12 @@ def create_splits(data, target, test_size, validation_size_from_test_size):
 
 
 
-def train_and_save_model(classifier, hyperparameter_list, X_train, y_train, X_validation, y_validation):
+def train_and_save_model(classifier, hyperparameter_list, X_train, y_train, X_validation, y_validation, X_test, y_test):
 
     accuracy_validation, f1_validation, model_location, non_skipped_values = [], [], [], []
+    hyperparameter_val_1, hyperparameter_val_2 = [], []
+    accuracy_train, f1_train = [], []
+    accuracy_test, f1_test = [], []
 
     for hyperparameter_value in hyperparameter_list: 
 
@@ -50,7 +53,7 @@ def train_and_save_model(classifier, hyperparameter_list, X_train, y_train, X_va
         if classifier == 'SVM':
             clf = svm.SVC(gamma = hyperparameter_value)
         elif classifier == 'DecisionTree':
-            clf = tree.DecisionTreeClassifier(max_depth = hyperparameter_value)
+            clf = tree.DecisionTreeClassifier(max_depth = hyperparameter_value[0], max_features = hyperparameter_value[1], criterion = hyperparameter_value[2])
 
         # Learn the digits on the train subset
         clf.fit(X_train, y_train)
@@ -61,24 +64,71 @@ def train_and_save_model(classifier, hyperparameter_list, X_train, y_train, X_va
         val_accuracy_score = accuracy_score(y_validation, predicted_val)
         val_f1_score = f1_score(y_validation, predicted_val, average='weighted')
 
-        if val_accuracy_score < 0.11:
-            print("Skipping for the hyperparameter value:", hyperparameter_value)
-            continue
+        # Predict the value of the digit on the test subset
+        predicted_train = clf.predict(X_train)
+
+        train_accuracy_score = accuracy_score(y_train, predicted_train)
+        train_f1_score = f1_score(y_train, predicted_train, average='weighted')
+
+        # Predict the value of the digit on the test subset
+        predicted_test = clf.predict(X_test)
+
+        test_accuracy_score = accuracy_score(y_test, predicted_test)
+        test_f1_score = f1_score(y_test, predicted_test, average='weighted')
 
         #Save the model to disk
         saved_model = pickle.dumps(clf)
         model_path = '/home/niladri/mlops/mnist-example/models'
-        path = model_path + '/' + classifier + '_' + str(hyperparameter_value) + '.pkl' 
+        path = model_path + '/' + classifier + '_' + str(hyperparameter_list[0]) + '_' + 'hyperparameter_list[2]' + '.pkl' 
         with open(path, 'wb') as f:
             pickle.dump(clf, f)
 
         
         accuracy_validation.append(val_accuracy_score)
         f1_validation.append(val_f1_score)
-        model_location.append(path)
-        non_skipped_values.append(hyperparameter_value)
 
-    df = pd.DataFrame(data = {'Hyperparameter Value': non_skipped_values ,'Accuracy of Validation Data': accuracy_validation, 'f1 score of Validation Data': f1_validation, 'Model Location': model_location})
+        accuracy_train.append(train_accuracy_score)
+        f1_train.append(train_f1_score)
+
+        accuracy_test.append(test_accuracy_score)
+        f1_test.append(test_f1_score)
+
+        print(f1_test)
+
+        model_location.append(path)
+        non_skipped_values.append(hyperparameter_value[0])
+        hyperparameter_val_1.append(hyperparameter_value[1])
+        hyperparameter_val_2.append(hyperparameter_value[2])
+
+    #df = pd.DataFrame(data = {'Max Depth ': non_skipped_values , 'Max Features ': hyperparameter_val_1, 'Criterion ': hyperparameter_val_2, 'Accuracy of Train Data': accuracy_train, 'f1 score of Train Data': f1_train,  'Accuracy of Validation Data': accuracy_validation, 'f1 score of Validation Data': f1_validation,  'Accuracy of Test Data': accuracy_test, 'f1 score of Test Data': f1_test,'Model Location': model_location})
+
+    #print(df)
+
+    df = pd.DataFrame(data = {'Max Depth ': non_skipped_values , 'Max Features ': hyperparameter_val_1, 'Criterion ': hyperparameter_val_2, 'Accuracy of Train Data': accuracy_train, 'f1 score of Train Data': f1_train,  'Model Location': model_location})
+    print(df)
+    print()
+    acc_df_mean = df['Accuracy of Train Data'].mean()
+    f1_df_mean = df['f1 score of Train Data'].mean()
+    print("Accuracy Mean of Train:", acc_df_mean)
+    print("f1 score Mean of Train:", f1_df_mean)
+    print()
+
+    df = pd.DataFrame(data = {'Max Depth ': non_skipped_values , 'Max Features ': hyperparameter_val_1, 'Criterion ': hyperparameter_val_2, 'Accuracy of Validation Data': accuracy_validation, 'f1 score of Validation Data': f1_validation,  'Model Location': model_location})
+    print(df)
+    print()
+    acc_df_mean = df['Accuracy of Validation Data'].mean()
+    f1_df_mean = df['f1 score of Validation Data'].mean()
+    print("Accuracy Mean of Validation:", acc_df_mean)
+    print("f1 score Mean of Validation:", f1_df_mean)
+    print()
+    df = pd.DataFrame(data = {'Max Depth ': non_skipped_values , 'Max Features ': hyperparameter_val_1, 'Criterion ': hyperparameter_val_2, 'Accuracy of Test Data': accuracy_test, 'f1 score of Test Data': f1_test,  'Model Location': model_location})
+    print(df)
+    print()
+    acc_df_mean = df['Accuracy of Test Data'].mean()
+    f1_df_mean = df['f1 score of Test Data'].mean()
+    print("Accuracy Mean of Test:", acc_df_mean)
+    print("f1 score Mean of Test:", f1_df_mean)
+    print()
 
     return df
 
@@ -110,6 +160,10 @@ def print_data_frame (clf_name, df):
     print()
     print("Metrics for:", clf_name)
     print(df)
+    acc_df_mean = df['Accuracy of Validation Data'].mean()
+    f1_df_mean = df['f1 score of Validation Data'].mean()
+    print("Accuracy Mean of Validation:", acc_df_mean)
+    print("f1 score Mean of Validation:", f1_df_mean)
 
 def print_metrics(model_accuracy, model_f1_score):
 
